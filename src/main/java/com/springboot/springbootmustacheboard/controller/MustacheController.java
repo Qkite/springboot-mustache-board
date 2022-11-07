5e30576ef822a55930d7938b8da32c34d01883bb
Qkite <yeonjigu4@gmail.com>
2022-11-07 11:46:26 +0900

    controller 생성 및 pathvariable을 이용해서 user hello 출력

---
+++ src/main/java/com/springboot/springbootmustacheboard/controller/MustacheController.java
@@ -0,0 +1,25 @@
+package com.springboot.springbootmustacheboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value="/hi")
+    public String mustacheCon1(Model model){
+        model.addAttribute("username", "user");
+        return "greetings";
+    }
+
+    @GetMapping(value="/hi/{id}")
+    public String mustacheCon2(Model model, @PathVariable String id){
+        model.addAttribute("username", "user");
+        model.addAttribute("id", id);
+        return "greetings";
+    }
+
+}
